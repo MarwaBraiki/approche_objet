b9485c01ade2b810e73588e5567a3a0a204876a2
MarwaBraiki <braikimarwa91@gmail.com>
2024-07-16 14:54:30 +0200

    Tp13_-COMPRENDRE L'INTERET DE LA CLASSE STRINGBUILDER_Version_1

---
+++ src/fr/diginamic/chaines/TestStringBuilder.java
@@ -0,0 +1,27 @@
+package fr.diginamic.chaines;
+
+public class TestStringBuilder {
+    public static void main(String[] args) {
+        // Chronométrage avec StringBuilder
+        long debutSB = System.currentTimeMillis();
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= 100000; i++) {
+            sb.append(i);
+        }
+
+        long finSB = System.currentTimeMillis();
+        System.out.println("Temps écoulé avec StringBuilder en millisecondes : " + (finSB - debutSB));
+
+        // Chronométrage avec String et l'opérateur +
+        long debutStr = System.currentTimeMillis();
+
+        String str = "";
+        for (int i = 1; i <= 100000; i++) {
+            str += i;
+        }
+
+        long finStr = System.currentTimeMillis();
+        System.out.println("Temps écoulé avec String en millisecondes : " + (finStr - debutStr));
+    }
+}
